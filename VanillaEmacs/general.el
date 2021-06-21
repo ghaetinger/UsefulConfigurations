a1428f9c5f48da976e857f9d7ec9838e1ae157d8
Guilherme Haetinger <ghaetinger@gmail.com>
2021-06-21 16:16:08 -0300

    :sparkles: kakoune keys and ligatures

--- VanillaEmacs/general.el
+++ VanillaEmacs/general.el
@@ -1,10 +1,20 @@
 (require 'general)
 
 (general-define-key
- "M-x" 'helm-M-x)
+ :keymaps 'ryo-modal-mode-map
+ :prefix "SPC"
+ "p" '(:keymap projectile-command-map :wk "projectile" :package projectile)
+ "b" (general-simulate-key "C-x" :which-key "buffer")
+ "SPC" 'helm-M-x
+)
 
 (general-define-key
- :keymaps 'normal
- :prefix "SPC"
- "p" '(:keymap projectile-command-map :wk "projectile prefix" :package projectile)
+ :keymaps 'ryo-modal-mode-map
+ :prefix "SPC f"
+ :which-key "file"
+ "f" 'find-file
+ "s" 'swiper
+ "w" 'save-buffer
  )
+
+(general-define-key "<escape>" 'ryo-modal-mode)
